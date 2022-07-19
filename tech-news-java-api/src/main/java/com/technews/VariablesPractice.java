61ed408fc6d74d3db3c7e01ac10855958b320788
Maria Meyer <mbean1216@icloud.com>
2022-07-19 15:01:34 -0500

    creating comment, post, user, and vote classes

---
+++ tech-news-java-api/src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,17 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int a = 4;
+        double b = 3.5;
+        String c = "cat";
+
+        double sum = a + b;
+
+        String concat = a + c;
+
+        System.out.println(sum);
+
+        System.out.println(concat);
+    }
+}
